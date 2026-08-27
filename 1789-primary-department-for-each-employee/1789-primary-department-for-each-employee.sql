e22ff276c9b04bea6b6a0623aeb9177044edfb6b
Vrushabh Patil <vrushabhpatil97711@gmail.com>
2026-08-28 00:05:59 +0530

    Time: 678 ms (28.9%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,10 +1,9 @@
 SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y'
-
-UNION
-
+FROM Employee 
+WHERE primary_flag='Y'
+UNION ALL
 SELECT employee_id, MIN(department_id)
-FROM Employee
-GROUP BY employee_id
-HAVING COUNT(*) = 1;
+FROM Employee 
+GROUP BY employee_id 
+HAVING count(*)=1;
+
